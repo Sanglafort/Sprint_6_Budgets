f4f59a1da742f9698e4c9cc95d4a8f7bb45927d8
Sanglafort <silviaangfort@gmail.com>
2024-01-14 13:39:24 +0100

    Service BudgetOptions OK

--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BudgetOptions } from '../interfaces/budgetOptions.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
 
-  constructor() { }
+  public budgetOptions: BudgetOptions[] = [
+    {
+      title: 'Seo',
+      description: "Programació d'una campanya de SEO completa",
+      price: 300
+    },
+    {
+      title: 'Ads',
+      description: "Programació d'una campanya de publicitat completa",
+      price: 400
+    },
+    {
+      title: 'Web',
+      description: "Programació d'una pàgina web completa",
+      price: 500
+    }
+
+  ]
 }
