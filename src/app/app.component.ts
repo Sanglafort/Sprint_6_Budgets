27c1760f611b6ab4580fc0b9a4ecc649356a3412
Sanglafort <silviaangfort@gmail.com>
2024-01-14 20:20:52 +0100

    Home (3 budgets) Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, WelcomeComponent],
+  imports: [CommonModule, RouterOutlet, WelcomeComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BudgetService } from '../services/budget.service';
+import { BudgetOptions } from '../interfaces/budgetOptions.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  public options: BudgetOptions[] = this.budgetOptions.budgetOptions;
+
+  public status: boolean [] = [false];
+
+  public checkBoxForm : FormGroup = this.fb.group({
+    box1: [false, [ Validators.required, Validators.requiredTrue ]],
+    box2: [false, [ Validators.required, Validators.requiredTrue ]],
+    box3: [false, [ Validators.required, Validators.requiredTrue ]],
+  });
+
+  constructor(
+    public fb: FormBuilder,
+    public budgetOptions: BudgetService
+    ) {}
 
 }
